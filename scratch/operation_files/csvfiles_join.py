4144b8c26dbf06715ce1cecb2fbe9b9afe1cf1de
Chinna Babu <noobycoder6921@gmail.com>
2022-04-03 10:58:45 +0530

    changed

--- scratch/operation_files/csvfiles_join.py
+++ scratch/operation_files/csvfiles_join.py
@@ -12,34 +12,35 @@ def joincsv(path):
         parameters
         ["TweetsData*.csv", 'CleanedDatav*.csv', '49*.csv']
     """
-    # fileexe = '49*.csv'
-    # # path to the folder/
-
-    folder_path = os.path.join(path, 'link\\*.csv')
     
+    # !--------------------------path to the folder-----------------------------
+    # folder_path = os.path.join(path, 'csv_files\\*.csv')
     # list of all files_path present in path folder
-    files_list = glob(folder_path)
+    # files_list = glob(folder_path)
     # print(files_list)
-    # files_list = [os.path.join(path, 'start_22_03_10T00_00_end_2022_03_24_T20_49.csv'),
-                #   os.path.join(path, 'lookup.csv'),
-                #   os.path.join(path, 'start_22_03_23T05_21_end_2022_03_14_T18_49v2.csv')]
+    #-------------------------------END----------------------------------------
 
-    # Joining csv files with concat, map
-    # converting all csv data to DataFrame with !read_csv
+    # ! ---------------- giving path of files directly---------------------------
+    files_list = [os.path.join(path, 'final_link.csv'),
+                  os.path.join(path, 'march.csv')]
+
+    # ? Joining csv files with concat, map
+    # ? converting all csv data to DataFrame with read_csv
     df = pd.concat(map(pd.read_csv, files_list), ignore_index=True)
-    # df.drop(columns='Unnamed: 0')
+    try:
+        df.drop(columns='Unnamed: 0',axis =1)
+    except:
+        pass
     df.info()
-    # # Removing Duplicate records
-    # # -----
+
+    # !Removing Duplicate records
     print("Before drop_duplicates: ", df.shape)
-    # # df2 = df.drop_duplicates(subset=['tweet_id', 'user_id', 'created_at', 'tweet'], keep='last', inplace=True, ignore_index=True)
-    # # print("No of duplicated rows :", df2.shape)
 
     df.drop_duplicates(subset=['tweet_id', 'user_id', 'created_at', 'tweet'], keep='last', inplace=True, ignore_index=True)
     print("After drop_duplicates: ", df.shape)
 
     # # writing to new single Csv file
-    df.to_csv(os.path.join(path, 'final_link.csv'),index=False)
+    df.to_csv(os.path.join(path, 'march14_31.csv'),index=False)
     # print('completed')
 
 
